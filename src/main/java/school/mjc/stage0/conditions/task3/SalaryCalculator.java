1363d5bc11945c1643ff4a996a10cc9032abaf0c
Islombek Yadgarov <islam.yadgarov.677@gmail.com>
2022-11-04 17:39:03 +0500

    "vav"

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        //Basic Salary <= 10000: taxes - 15%. 10000 < Basic Salary <= 20000: taxes - 18% Basic Salary > 20000 : taxes - 20% Basic salary < 0
+
+        if (salary <= 10000){
+            int taxes = (int) (salary*0.15);
+            System.out.println(salary-taxes);
+        } 
+        else if (10000 < salary || salary<=20000) {
+            int taxes = (int) (salary*0.18);
+            System.out.println(salary-taxes);
+        } else if (salary > 20000) {
+            int taxes = (int) (salary*0.20);
+            System.out.println(salary-taxes);
+        } else if (salary < 0) {
+            System.out.println("wrong input!");
+        }
     }
 }
